0890ead168bc1e9a0094ce3a91dc0797850baba3
joshkrollz <joshua.karolus@cuw.edu>
2015-12-01 16:29:44 -0600

    second commit

--- Exit.java
+++ Exit.java
@@ -1,4 +1,22 @@
 
-public class Exit {
+public class Exit 
+{
+	private String name;
+	private int destinationID;
+	
+	public Exit(JSONObject obj)
+	{
+		this.name = ((JSONStringVariable)obj.getVariableForName("name")).getValue();
+		this.destinationID = ((JSONNumberVariable)obj.getVariableForName("destinationID")).getValue();
+	}
 
+	public String getName() 
+	{
+		return name;
+	}
+
+	public int getDestinationID() 
+	{
+		return destinationID;
+	}
 }
--- JSONArray.java
+++ JSONArray.java
@@ -1,4 +1,5 @@
 
-public class JSONArray {
+public class JSONArray 
+{
 
 }
